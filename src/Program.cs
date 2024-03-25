8bbdcd62e3d385534210b7520ecfe3ddb243f92e
illyum <90023277+itzilly@users.noreply.github.com>
2024-03-25 04:21:40 -0600

    Add default config interface

---
+++ src/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program {
+    private static string TOKEN;
+    static void Main(string[] args) {
+        TOKEN = Environment.GetEnvironmentVariable("TOKEN") ?? string.Empty;
+        Console.WriteLine("Token: " + TOKEN);
+    }
+}
---
+++ src/util/IBotConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace ActivityTracker.src.util;
+
+public interface IBotConfig : INotifyPropertyChanged {
+    int Id { get; set;  }
+    
+    string HypixelGuildId { get; set; }
+    
+    string HypixelApiKey { get; set; }
+    
+    [DefaultValue(1)]
+    int FetchRateMinutes { get; set; }
+    
+    IEnumerable<string> ExtraPlayers { get; }
+}
