ccbb6ecb0d976b7ff1b32e7e76e42f8eeed9b6e5
illyum <90023277+itzilly@users.noreply.github.com>
2024-03-25 04:32:30 -0600

    Create /ping command

--- src/SlashCommands.cs
+++ src/SlashCommands.cs
@@ -1,7 +1,12 @@
-﻿using DSharpPlus.SlashCommands;
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
 
 namespace ActivityTracker.src;
 
 public class SlashCommands : ApplicationCommandModule {
-    
+    [SlashCommand("ping", "Replies to command")]
+    public async Task PingSlashCommand(InteractionContext ctx) {
+        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent("Pong!"));
+    }
 }
