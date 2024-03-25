80215d1b0581698ca8ad1af1b97392764be3bcfc
illyum <90023277+itzilly@users.noreply.github.com>
2024-03-25 04:22:06 -0600

    Add ActivityTracker bot

---
+++ src/ActivityTrackerBot.cs
@@ -0,0 +1,75 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using ActivityTracker.src.util;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.EventArgs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ActivityTracker.src;
+
+public class ActivityTrackerBot {
+    private string TOKEN;
+
+    public DiscordClient DiscordBot { get; private set; }
+    public SlashCommandsExtension SlashCommands { get; private set; }
+    public IBotConfig BotConfig { get; private set; }
+    private readonly Metadat metadat = new();
+
+    public ActivityTrackerBot(string token, IBotConfig config) {
+        TOKEN = token;
+        BotConfig = config;
+    }
+
+    public async Task RunBotAsync() {
+        DiscordConfiguration defaultConfiguration = new DiscordConfiguration {
+            AutoReconnect = true,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+            LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt",
+            MinimumLogLevel = LogLevel.Debug,
+            Token = TOKEN,
+            TokenType = TokenType.Bot,
+            LogUnknownEvents = true
+        };
+        DiscordBot = new DiscordClient(defaultConfiguration);
+        DiscordBot.SessionCreated += OnReadyEvent;
+
+        var slashCommandConfig = new SlashCommandsConfiguration {
+            Services = new ServiceCollection().AddSingleton(BotConfig).BuildServiceProvider()
+        };
+        SlashCommands = DiscordBot.UseSlashCommands(slashCommandConfig);
+        SlashCommands.RegisterCommands<SlashCommands>();
+        SlashCommands.SlashCommandErrored += OnSlashCommandError;
+
+        await SetDefaultStatus();
+
+        await DiscordBot.ConnectAsync();
+        await Task.Delay(-1);
+    }
+
+    private async Task OnReadyEvent(DiscordClient discordClient, SessionReadyEventArgs eventArgs) {
+        discordClient.Logger.LogInformation($"{metadat.BotName} - {metadat.VersionName} has established session");
+    }
+
+    private async Task OnSlashCommandError(SlashCommandsExtension sender, SlashCommandErrorEventArgs eventArgs) {
+        if (eventArgs.Exception is SlashExecutionChecksFailedException) {
+            await eventArgs.Context.CreateResponseAsync(new DiscordEmbedBuilder()
+                .WithColor(new DiscordColor("#FF0000"))
+                .WithDescription(":x: You don't have permission for this command! :x:")
+                .WithTimestamp(DateTimeOffset.Now)
+                .WithFooter($"{metadat.BotName} | {metadat.VersionName}")
+                .Build()
+            );
+            return;
+        }
+
+        DiscordBot.Logger.LogError(eventArgs.Exception.ToString());
+        DiscordBot.Logger.LogError($"{eventArgs.Exception.Message} | {eventArgs.Exception.StackTrace}");
+    }
+
+    private async Task SetDefaultStatus() {
+        DiscordActivity activity = new DiscordActivity("guild players!", ActivityType.Watching);
+        await DiscordBot.UpdateStatusAsync(activity);
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,66 @@
-﻿class Program {
+﻿using ActivityTracker.src;
+using Serilog;
+using Config.Net;
+using Serilog.Formatting.Json;
+using ActivityTracker.src.util;
+using DSharpPlus;
+
+
+class Program {
     private static string TOKEN;
+
+    private static int LOG_FILE_SIZE_LIMIT = 20_000_000; // 20MB
+    
     static void Main(string[] args) {
+        CreateLogger();
+        
+        IBotConfig config = new ConfigurationBuilder<IBotConfig>()
+            .UseJsonFile("conf")
+            .Build();
+
+        // Set default
+        // (Writing a value will generate the file) 
+        config.Id = 1;
+
+        // TODO: Move this into the bot startup logic
         TOKEN = Environment.GetEnvironmentVariable("TOKEN") ?? string.Empty;
-        Console.WriteLine("Token: " + TOKEN);
+        if (TOKEN == string.Empty) {
+            Log.Error("No discord token found");
+            Close(401);
+            // throw new ArgumentNullException("TOKEN", "Invalid Argument");
+        }
+
+        ActivityTrackerBot activityTrackerBot = new ActivityTrackerBot(TOKEN, config);
+        activityTrackerBot.RunBotAsync().GetAwaiter().GetResult();
+        
+        Close();
+    }
+
+    static void CreateLogger() {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File(
+                path: "latest.log",
+                fileSizeLimitBytes: LOG_FILE_SIZE_LIMIT,
+                retainedFileCountLimit: 100,
+                shared: true,
+                formatter: new JsonFormatter()
+            )
+            .MinimumLevel.Debug()
+            .CreateLogger();
+        
+        Log.Debug("logger created");
+    }
+    
+    static void Close() {
+        Log.Debug("closing logger");
+        Log.CloseAndFlush();
+        Environment.Exit(0);
+    }
+
+    static void Close(int exitCode) {
+        Log.Debug("closing logger and terminating process");
+        Log.CloseAndFlush();
+        Environment.Exit(exitCode);
     }
 }
